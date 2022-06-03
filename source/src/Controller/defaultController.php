b4287f6f24f87bb00b2f2b421b6a8c36b97f2244
Kot1899 <romanenko.vit.kharkiv@gmail.com>
2022-06-03 19:01:36 +0300

    link about

--- source/src/Controller/defaultController.php
+++ source/src/Controller/defaultController.php
@@ -12,10 +12,30 @@ use \Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class defaultController extends AbstractController{
     /**
-     * @Route("/", name="second_index")
+     * @Route("/", name="default_index")
      */
     public function index(){
-        return $this->render('default/index.html.twig');
+        $a=10;
+        $b=20;
+        $userName='Alex';
+        $c=$a+$b;
+        $users=[
+            ['id'=>1, 'name'=>'Alex'],
+            ['id'=>2, 'name'=>'Mike'],
+            ['id'=>3, 'name'=>'Kile']
+        ];
+        return $this->render('default/index.html.twig',
+            [
+            'c'=>$c,
+            'userName'=>$userName,
+                'users'=>$users,
+        ]);
+    }
+    /**
+     * @Route("/about", name="default_about")
+     */
+    public function about(){
+        return $this->render ('default/about.html.twig');
     }
 }
 
